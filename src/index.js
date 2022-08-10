d586d7176c450f4917e163b84ed69ef234a4f325
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-10 08:00:10 +0530

    Modifying Middleware

--- src/index.js
+++ src/index.js
@@ -10,14 +10,22 @@ import rootReducer from './reducers.js';
 
 //function logger(obj,next,action)
 //logger(obj)(next)(action)
-const logger = function({dispatch,getState }){
-    return function(next){
-         return function(action){
+// const logger = function({dispatch,getState }){
+//     return function(next){
+//          return function(action){
+//           //middleware code
+//             console.log('ACTION_TYPE =', action.type);
+//             next(action);
+//           }
+//     }
+// }
+
+
+
+const logger = ({dispatch,getState }) => (next) => (action) =>{
           //middleware code
             console.log('ACTION_TYPE =', action.type);
             next(action);
-          }
-    }
 }
 
 
